00fc52aa8c638d98779dacb29fb74c8234d9a9da
Nur Aqilah Hannan Binti Azmi <nuraqilahhannan0912@gmail.com>
2022-03-10 23:19:20 +0800

    Initial Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,16 @@
+package samples.demo;
+
+public class Location {
+
+	private int homework;
+
+	public int getHomework() {
+		return homework;
+	}
+
+	public void setHomework(int homework) {
+		this.homework = homework;
+	}
+	
+	
+	}
